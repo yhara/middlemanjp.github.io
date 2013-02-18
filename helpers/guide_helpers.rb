4a3b69094a603d412b7da2b6883c608b4105d1b8
yterajima <terra@e2esound.com>
2013-02-18 21:58:40 +0900

    Update index's title and tracking code
    - index のタイトルをわかりやすく変更
    - google analytics のコードを新規に取得し設置

--- helpers/guide_helpers.rb
+++ helpers/guide_helpers.rb
@@ -1,3 +1,4 @@
+# -*- encoding: utf-8 -*-
 require "pathname"
 
 module GuideHelpers
@@ -6,38 +7,38 @@ module GuideHelpers
     if data.page.title
       title << data.page.title
     else
-      title << "Hand-crafted frontend development"
+      title << "効率的な作業を可能にする Ruby 製の静的サイト生成ツール"
     end
     title
   end
-  
+
   def edit_guide_url
     p = Pathname(current_page.source_file).relative_path_from(Pathname(root))
     "https://github.com/middleman/middleman-guides/blob/master/#{p}"
   end
-  
+
   def pages_for_group(group_name)
     group = data.nav.find do |g|
       g.name == group_name
     end
-      
+
     pages = []
-    
+
     return pages unless group
-    
+
     if group.directory
       pages << sitemap.resources.select { |r|
         r.path.match(%r{^#{group.directory}}) && !r.data.hidden
       }.map do |r|
         ::Middleman::Util.recursively_enhance({
           :title => r.data.title,
-          :path  => r.url 
+          :path  => r.url
         })
       end.sort_by { |p| p.title }
     end
-    
+
     pages << group.pages if group.pages
-    
+
     pages.flatten
   end
 end
